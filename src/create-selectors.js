ae8b6b61b2e039391300a90351505dc80d6e1a95
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-04-24 14:20:16 +0800

    GREEN: create-selectors.js > simple properties > creates a selector for a simple property

--- src/create-selectors.js
+++ src/create-selectors.js
@@ -1,7 +1,17 @@
+function createSelectorName(selectorName) {
+    return `select${selectorName.charAt(0).toUpperCase()}${selectorName.slice(1)}`;
+}
+
 function createSelectors(selectorSpecification) {
     const selectors = {
         selectState: selectorSpecification._selector ?? ((state) => state)
     };
+
+    for (const [key, value] of Object.entries(selectorSpecification)) {
+        if (value['_export'] === true) {
+            selectors[createSelectorName(key)] = (state) => state[key];
+        }
+    }
 
     return selectors;
 }
--- src/createSelectors.js
+++
@@ -1,9 +0,0 @@
-function createSelectors(selectorFunctions) {
-    const selectors = selectorFunctions;
-
-    selectors.selectState = (state) => state;
-
-    return selectors;
-}
-
-export default createSelectors;
