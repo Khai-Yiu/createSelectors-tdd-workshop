ace4d1638c4e26aef9d64e47d261585cdbd751d2
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-04-24 15:02:08 +0800

    create-selectors.js > simple properties > returns a default value for a simple boolean property

--- src/create-selectors.js
+++ src/create-selectors.js
@@ -1,15 +1,26 @@
+import R from 'ramda';
+
 function createSelectorName(selectorName) {
     return `select${selectorName.charAt(0).toUpperCase()}${selectorName.slice(1)}`;
 }
 
 function createSelectors(selectorSpecification) {
     const selectors = {
-        selectState: selectorSpecification._selector ?? ((state) => state)
+        selectState: selectorSpecification._selector ?? R.identity
     };
 
     for (const [key, value] of Object.entries(selectorSpecification)) {
         if (value['_export'] !== false) {
-            selectors[createSelectorName(key)] = (state) => state[key];
+            selectors[createSelectorName(key)] = (state) => {
+                if (
+                    !state.hasOwnProperty(key) &&
+                    value.hasOwnProperty('_default')
+                ) {
+                    return value['_default'];
+                }
+
+                return state[key];
+            };
         }
     }
 
